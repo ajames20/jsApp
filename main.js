b1bde061a05c4514dde8dd1293ac2f2d132da4f3
Andrew James <ajames20@gmail.com>
2017-03-16 12:06:24 +0200

    jsApp complete

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+    let issueDesc = document.getElementById('issueDescInput').value;
+    let issueSeverity = document.getElementById('issueSeverityInput').value;
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    let issueId = chance.guid();
+    let issueStatus = 'Open';
+
+    let issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if (localStorage.getItem('issues') == null) {
+        let issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        let issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues))
+    }
+
+    document.getElementById('issueInputForm').reset();
+    fetchIssues();
+    e.preventDefault();
+}
+
+function setStatusClosed(id) {
+    let issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (let i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues[i].status = 'Closed';
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
+
+function deleteIssue(id) {
+    let issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (let i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
+
 function fetchIssues() {
     let issues = JSON.parse(localStorage.getItem('issues'));
     let issuseList = document.getElementById('issuesList');
@@ -13,12 +71,13 @@ function fetchIssues() {
 
         issuseList.innerHTML += '<div class="well">' +
             '<h6>Issue Id: ' + id + '</h6>' +
-            '<p><span class="label label-info>"' + status + '</span></p>' +
+            '<p><span class="label label-info">' + status + '</span></p>' +
             '<h3>' + description + '</h3>' +
             '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
-            '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + '</p>' +
-            '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning">Close</a>' +
-            '<a href="#" onclick="deleteIssue(\'' + id + '\')" class="btn btn-danger">Delete</a>' +
+            '<p><span class="glyphicon glyphicon-user"></span>' + ' ' + assignedTo + '</p>' +
+            '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' + id + '\')">Close</a> ' +
+            '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' + id + '\')">Delete</a>' +
             '</div>'
+
     }
 }
